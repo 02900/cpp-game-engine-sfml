fb6e2a082266f94d9a364790d3f2c0c658ac452e
Juan <juan@dd3tech.mx>
2024-02-12 23:16:44 -0600

    init

--- Makefile
+++ Makefile
@@ -6,11 +6,11 @@ SFML_DIR	:= /opt/homebrew/Cellar/sfml/2.6.1
 
 # compiler and linker flags
 CXX_FLAGS	:= -O3 -std=c++20 -Wno-unused-result
-INCLUDES	:= -I ./src -I ./src/imgui -I ${SFML_DIR}/include -I
+INCLUDES	:= -I ./src -I ./src/imgui-1.90.1 -I ./src/imgui-sfml-2.6 -I ${SFML_DIR}/include
 LDFLAGS		:= -O3 -L ${SFML_DIR}/lib -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -framework OpenGL
 
 # the source files for the ecs game engine
-SRC_FILES	:= $(wildcard src/*.cpp src/imgui/*.cpp)
+SRC_FILES	:= $(wildcard src/*.cpp src/imgui-1.90.1/*.cpp src/imgui-sfml-2.6/*.cpp)
 OBJ_FILES	:= $(SRC_FILES:.cpp=.o)
 
 # all of these targets will be made if you just type 'make'
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,106 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <memory>
+#include <fstream>
 
-int main()
+#include <SFML/Graphics.hpp>
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+int main(int argc, char *argv[])
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    const int wWidth = 800;
+    const int wHeight = 600;
+    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML works!");
+    window.setFramerateLimit(60);
+
+    ImGui::SFML::Init(window);
+    sf::Clock deltaClock;
+
+    ImGui::GetStyle().ScaleAllSizes(1.0f);
+
+    float c[3] = {0.f, 1.f, 1.f};
+
+    // example circle shape
+    float circleRadius = 50.f;
+    int circleSegments = 32;
+    float circleSpeedX = 1.0f;
+    float circleSpeedY = 0.5f;
+    bool drawCircle = true;
+    bool drawText = true;
+
+    sf::CircleShape circle(circleRadius, circleSegments);
+    circle.setPosition(wWidth / 2, wHeight / 2);
+
+    sf::Font myFont;
+    if (!myFont.loadFromFile("bin/fonts/font.ttf"))
+    {
+        std::cout << "Error loading font\n";
+        exit(-1);
+    }
+
+    sf::Text text("Hello World", myFont, 30);
+    text.setPosition(0, wHeight - (float)text.getCharacterSize());
+
+    char displayString[255] = "Sample Text";
+
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
+            ImGui::SFML::ProcessEvent(window, event);
+
             if (event.type == sf::Event::Closed)
                 window.close();
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                std::cout << "Key pressed: " << event.key.code << std::endl;
+
+                if (event.key.code == sf::Keyboard::X)
+                {
+                    circleSpeedX *= -1.0f;
+                }
+            }
         }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Sample window");
+        ImGui::Text("Hello World");
+        ImGui::Checkbox("Draw Circle", &drawCircle);
+        ImGui::SameLine();
+        ImGui::Checkbox("Draw Text", &drawText);
+        ImGui::SliderFloat("Circle Radius", &circleRadius, 0.0f, 100.0f);
+        ImGui::SliderInt("Circle Segments", &circleSegments, 3, 64);
+        ImGui::ColorEdit3("Color Circle", c);
+        ImGui::InputText("Display String", displayString, 255);
+
+        if (ImGui::Button("Set Text"))
+            text.setString(displayString);
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Reset Circle"))
+            circle.setPosition(0, 0);
+
+        ImGui::End();
+
+        circle.setFillColor(sf::Color(c[0] * 255, c[1] * 255, c[2] * 255));
+        circle.setPointCount(circleSegments);
+        circle.setRadius(circleRadius);
+
+        circle.setPosition(circle.getPosition().x + circleSpeedX, circle.getPosition().y + circleSpeedY);
+
         window.clear();
-        window.draw(shape);
+        if (drawCircle)
+            window.draw(circle);
+
+        if (drawText)
+            window.draw(text);
+
+        ImGui::SFML::Render(window);
+
         window.display();
     }
     return 0;
